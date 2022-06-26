777bb1ab721ad32d13ec78d6bbbf4f86fa7d47df
Hasan Sefa Degirmenci <hdegirmenci18@ku.edu.tr>
2022-06-27 00:43:03 +0200

    change version

--- rocksdb/util/build_version.cc
+++ rocksdb/util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:89337d111659f07c2b53be7540db40b2c2793d41";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:2022-05-10";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:7fab3f7ed57006cbbf248b4a491170b3b983cd83";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2022-06-20";
 const char* rocksdb_build_compile_date = __DATE__;
